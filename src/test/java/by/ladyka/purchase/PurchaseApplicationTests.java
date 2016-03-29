939106eac387629ad88938afa3810cce9028f072
AndreiLadyka <andrei@ladyka.tk>
2016-03-29 16:00:53 +0300

    Angular2JS add

--- src/test/java/by/ladyka/purchase/PurchaseApplicationTests.java
+++ src/test/java/by/ladyka/purchase/PurchaseApplicationTests.java
@@ -2,11 +2,9 @@ package by.ladyka.purchase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = PurchaseApplication.class)
